d95bd937afca5e4e392e59de8e865ed5ec588731
Funky-Valentine <126513139+Funky-Valentine@users.noreply.github.com>
2023-03-07 20:44:16 +0000

    LOTA Main sprite
    Created main play area for LOTA and moved script to correct folder

--- Assets/Scripts/MinigameScripts/LOTA/LadyAndTheAmulet.cs
+++ Assets/Scripts/MinigameScripts/LOTA/LadyAndTheAmulet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LadyAndTheAmulet : MonoBehaviour
+public class LadyAndTheAmulet : BaseMinigame
 {
     // Start is called before the first frame update
     void Start()
